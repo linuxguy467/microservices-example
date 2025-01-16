ebbe67dfce8e62d0a83329a9ec0e4b00bd8a9651
linuxguy467 <me@matthem.codes>
2025-01-16 18:18:32 -0500

    add example user service

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import { ServiceBroker } from "moleculer";
-
-const broker = new ServiceBroker();
-
-// Greeter Service
-broker.createService({
-  name: "greeter",
-  actions: {
-    sayHello(ctx) {
-      return `Hello ${ctx.params.name}`;
-    },
-  },
-});
+import UserService from './services/user.service.js';
 
 async function startApp() {
-  await broker.start();
-  const res = await broker.call('greeter.sayHello', { name: 'John' });
-  console.log(res);
-  broker.stop();
+  // Start services
+  await UserService.start();
+
+  try {
+    // Simulate user creation
+    const newUser = await UserService.call('user.createUser', {
+      username: 'John',
+      email: 'john@gmail.com'
+    });
+    console.log('New User Created: ', newUser);
+    const users = await UserService.call('user.getUsers');
+    console.log('All Users: ', users);
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    // Stop services
+    await UserService.stop();
+  }
 }
 
 startApp();
